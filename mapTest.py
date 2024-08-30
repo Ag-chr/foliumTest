792cc7b935d4fd9524647d34ca35c2b4720ced57
Ag-chr <agc@aktivit.dk>
2024-08-30 15:01:21 +0200

    lavet line til alle punkterne i longboi

--- mapTest.py
+++ mapTest.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import folium as F
-longboi = pd.read_csv("LongBoi.csv")
+csvFile = pd.read_csv("LongBoi.csv")
+
 #53.0765452103364, 8.820692530590472
-map = F.Map(location=(53.08, 8.8), zoom_start=12)
+map = F.Map(location=(csvFile.iloc[0]["latitude"], csvFile.iloc[0]["longitude"]), zoom_start=12)
+F.Marker((csvFile.iloc[0]["latitude"], csvFile.iloc[0]["longitude"]), popup='Binaur').add_to(map)
+points = []
 
-F.Marker((53.08, 8.8), popup='Binaur').add_to(map)
+for i in range(len(csvFile)):
+    points.append((csvFile.iloc[i]["latitude"], csvFile.iloc[i]["longitude"]))
 
-F.PolyLine(locations=[(16.350000, 81.050000), (26.383333, 80.166667)], color='blue').add_to(map)
+F.PolyLine(locations=points, color='blue', popup="William").add_to(map)
+
 map.show_in_browser()
 map.save("Map.html")
-input("wait for exit")
